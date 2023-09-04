2533a76f2c6203d073604ee682d547c74659150e
aho <matsue4692@gmail.com>
2023-09-04 22:17:39 +0900

    Mod: index.tsx notionAPIを叩いて値を取得できることを確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import Head from "next/head";
+import { getAllPosts } from "../../lib/notionAPI";
 
-const inter = Inter({ subsets: ["latin"] });
+export const getStaticProps = async () => {
+  const allPosts = await getAllPosts();
+  return {
+    props: { allPosts },
+    revalidate: 60,
+  };
+};
 
-export default function Home() {
+export default function Home({ allPosts }) {
+  console.log(allPosts);
   return (
     <div>
       <Head>
