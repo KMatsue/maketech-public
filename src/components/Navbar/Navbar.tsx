c37f26147fb698dbf6f5ae6f55da37c6be50e2c6
aho <matsue4692@gmail.com>
2023-09-06 22:37:29 +0900

    Add: Layout.tsx Navbar.tsx ナビゲーションバーを実装

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="container mx-auto lg:px-2 px-5 lg:w-2/5">
+      <div className="container flex items-center justify-between mx-auto">
+        <Link href="/" className="text-2xl font-medium">
+          MakeCode
+        </Link>
+        <div>
+          <ul className="flex items-center py-4 text-sm">
+            <li>
+              <Link href="/" className="block px-4 py-2 hover:text-sky-900">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="block px-4 py-2 hover:text-sky-900">
+                𝕏
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="block px-4 py-2 hover:text-sky-900">
+                Zenn
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
