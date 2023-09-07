268e85239930fd10a1e1938e9dab85b0f0a387d8
aho <matsue4692@gmail.com>
2023-09-07 20:43:32 +0900

    Fix: SinglePost.tsx css修正

--- src/components/Post/SinglePost.tsx
+++ src/components/Post/SinglePost.tsx
@@ -16,7 +16,7 @@ const SinglePost = (props: Props) => {
 
   return (
     <>
-      <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 translate-all duration-300">
+      <section className="lg:w-2/3 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 translate-all duration-300">
         <div className="flex items-center gap-3">
           <h2 className="text-gray-100 text-2xl font-medium mb-2">
             <Link href={`/posts/${slug}`}>{title}</Link>
