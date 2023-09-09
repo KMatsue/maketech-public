f49695cd89c5aa1502672f528edf6500ce3cf113
aho <matsue4692@gmail.com>
2023-09-09 12:48:58 +0900

    Add: webpack.config.ts StorybookでTailwindCSSを読み込めるよう設定

---
+++ src/stories/Navbar.stories.ts
@@ -0,0 +1,16 @@
+import Navbar from "@/components/Navbar/Navbar";
+import type { Meta, StoryObj } from "@storybook/react";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "Components/Navbar",
+  component: Navbar,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+  },
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
+// export const navbarComponent = () => <Navbar />;
