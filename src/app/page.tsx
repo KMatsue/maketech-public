06a175841146d6a490522c897918ffa5f538343f
aho <matsue4692@gmail.com>
2023-09-19 21:36:48 +0900

    Add: page.tsx app/page.tsxファイルを追加しpages/index.tsxの内容を記述。index.tsxは削除

---
+++ src/app/page.tsx
@@ -0,0 +1,60 @@
+import { getAllTags, getPostsForTopPage } from "@/lib/notionAPI";
+import SinglePost from "@/components/Post/SinglePost";
+import { GetStaticProps } from "next";
+import Link from "next/link";
+import Tag from "@/components/Tags/Tags";
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   const fourPosts = await getPostsForTopPage(4);
+//   const allTags = await getAllTags();
+//   return {
+//     props: { fourPosts, allTags },
+//     revalidate: 60,
+//   };
+// };
+
+type Post = {
+  title: string;
+  description: string;
+  date: string;
+  tags: string[];
+  slug: string;
+};
+
+const Home = async () => {
+  const fourPosts = await getPostsForTopPage(4);
+  const allTags = await getAllTags();
+
+  return (
+    <main className="container mx-auto w-full mt-14 md:flex lg:w-8/12">
+      <div className="flex-1 md:flex-auto md:w-8/12 lg:w-9/12">
+        <h2 className="border-b-2 border-gray-500 mb-4 mx-4">Recent Posts</h2>
+        {fourPosts.map((post: Post, index: number) => (
+          <div key={index} className="mx-4">
+            <SinglePost
+              title={post.title}
+              description={post.description}
+              date={post.date}
+              tags={post.tags}
+              slug={post.slug}
+              isPaginationPage={false}
+            />
+          </div>
+        ))}
+        <h4>
+          <Link
+            href="/posts/page/1"
+            className="mb-6  mx-auto px-5 block text-right"
+          >
+            ....もっと見る
+          </Link>
+        </h4>
+      </div>
+      <div className="flex-1 md:flex-auto md:w-4/12 lg:w-3/12">
+        <h2 className="border-b-2 border-gray-500 mb-4 mx-4">Tags</h2>
+        <Tag tags={allTags} />
+      </div>
+    </main>
+  );
+};
+export default Home;
