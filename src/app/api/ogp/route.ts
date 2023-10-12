891d2719d367a76208002140ea61d981254b2fc5
aho <matsue4692@gmail.com>
2023-10-12 11:12:09 +0900

    Improve: route.ts

--- src/app/api/ogp/route.ts
+++ src/app/api/ogp/route.ts
@@ -1,5 +1,6 @@
 // import type { NextApiRequest, NextApiResponse } from "next";
-// import type { OgpData } from "@/types/ogpData";
+import { getOgp } from "@/lib/ogp";
+import type { OgpData } from "@/types/ogpData";
 
 // import { getOgp } from "@/lib/ogp";
 
@@ -13,3 +14,11 @@
 
 //   res.status(200).json(ogp);
 // }
+
+async function requestHandler(_request: Request) {
+  const url = _request.url;
+  const ogp = await getOgp(url);
+  return Response.json({ ogp });
+}
+
+export { requestHandler as GET };
