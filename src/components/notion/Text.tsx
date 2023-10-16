e99422666770050a6995ae28455bc93dc58953f0
aho <matsue4692@gmail.com>
2023-10-16 21:28:38 +0900

    Improve: Text.tsx

--- src/components/notion/Text.tsx
+++ src/components/notion/Text.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from "react";
-import styles from "@/app/posts/[slug]/post.module.css";
 import type { RichTextItemResponse } from "@notionhq/client/build/src/api-endpoints";
 
 type Props = {
@@ -14,7 +13,6 @@ export const Text: FC<Props> = ({ text }) => {
     return <span className="block h-6" />;
   }
   return text.map((value) => {
-    // console.log(value);
     if ("text" in value) {
       const {
         annotations: { bold, code, color, italic, strikethrough, underline },
@@ -23,11 +21,13 @@ export const Text: FC<Props> = ({ text }) => {
       return (
         <span
           className={[
-            bold ? styles.bold : "",
-            code ? styles.code : "",
-            italic ? styles.italic : "",
-            strikethrough ? styles.strikethrough : "",
-            underline ? styles.underline : "",
+            bold ? "font-bold" : "",
+            code
+              ? "bg-slate-200 dark:bg-slate-500 text-red-500 px-1 rounded-sm"
+              : "",
+            italic ? "italic" : "",
+            strikethrough ? "line-through" : "",
+            underline ? "underline" : "",
           ].join(" ")}
           style={color !== "default" ? { color } : {}}
           key={content}
