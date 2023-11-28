d3f13c9a7e3a685469a75085d70aa8abfb4e1195
aho <matsue4692@gmail.com>
2023-11-28 19:09:34 +0900

    Add: fonts.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import Providers from "@/components/Providers";
+import { inter } from "@/app/ui/fonts";
 
 export const metadata = {
   title: { default: "MaKe Tech", template: "%s | Make Lab Blog" },
@@ -9,7 +10,9 @@ export const metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
-      <body className="bg-neutral-50 dark:bg-neutral-950 max-w-7xl mx-auto xl:w-full px-2">
+      <body
+        className={`${inter.className} antialiased bg-neutral-50 dark:bg-neutral-950 max-w-7xl mx-auto xl:w-full px-2`}
+      >
         <Providers>
           <Navbar />
           <main className="relative z-10 mb-16 bg-neutral-50 dark:bg-neutral-950">
