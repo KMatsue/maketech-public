1432c1dca45ce3aafa469ea4519ea64f69f11926
aho <matsue4692@gmail.com>
2024-06-08 01:09:23 +0900

    Update: package.json プロジェクト名変更に伴い更新

--- src/app/posts/tag/[tag]/page/[page]/page.tsx
+++ src/app/posts/tag/[tag]/page/[page]/page.tsx
@@ -56,17 +56,20 @@ const BlogTagPageList = async ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container w-full mt-14 mx-auto">
-        <div className=" md:flex">
-          <div className="flex-1 md:flex-auto md:w-8/12 md:mr-4 lg:w-9/12">
-            <h2 className="border-b-2 border-gray-500 mb-4">
-              {`Search tags in ${currentTag}`}
-            </h2>
-            <section className="md:grid grid-cols-2 gap-3">
+      <main className="container w-full mt-14 mx-auto px-4 md:px-8 lg:px-16">
+        <header className="text-center my-8">
+          <h1 className="text-4xl font-bold mb-4">
+            {`Search tags in ${upperCaseCurrentTag}`}
+          </h1>
+        </header>
+        <div className="md:flex">
+          <div className="md:w-8/12 lg:w-9/12">
+            <h2 className="border-b-2 border-gray-500 mb-4">Posts</h2>
+            <section className="grid gap-4 md:gap-6 grid-cols-1 md:grid-cols-2">
               <Posts posts={posts} />
             </section>
           </div>
-          <div className="flex-1 md:flex-auto md:w-4/12 lg:w-3/12">
+          <div className="md:w-4/12 lg:w-3/12 mt-8 md:mt-0 md:ml-8">
             <h2 className="border-b-2 border-gray-500 mb-4">Tags</h2>
             <Tags tags={allTags} />
           </div>
