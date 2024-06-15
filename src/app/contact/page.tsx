f2277e9e99febcae4f7e6d342e9d7b79bc1c5515
aho <matsue4692@gmail.com>
2024-06-15 17:42:44 +0900

    Improve: contact/page.tsx コンタクトページ更新

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import ContactForm from "@/components/Contact/ContactForm";
 
-const Contact = () => {
-  return <div className="container  flex justify-center mx-auto">工事中</div>;
+const ContactPage = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold text-center mb-8">お問い合わせ</h1>
+      <p className="text-lg text-center mb-6">
+        ご質問やフィードバックがございましたら、下記のフォームからお問い合わせください。
+      </p>
+      <div className="max-w-lg mx-auto">
+        <ContactForm />
+      </div>
+    </div>
+  );
 };
 
-export default Contact;
+export default ContactPage;
