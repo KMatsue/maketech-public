e1375bb1ab7f1a8364fe0169b6078ba795d64ab3
aho <matsue4692@gmail.com>
2024-10-06 12:37:01 +0900

    Improve: layout.tsx
    - CookieConsetの実装
    - 現時点では採用せずコメント化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import { Metadata } from "next";
+// import CookieConsent from "react-cookie-consent";
 import "@/styles/globals.css";
+import { inter } from "@/app/ui/fonts";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import Providers from "@/components/Providers";
-import { inter } from "@/app/ui/fonts";
-import { Metadata } from "next";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 export const revalidate = 300;
@@ -56,6 +57,17 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           <main className="flex-grow relative z-10 mb-16">{children}</main>
           <Footer />
           <GoogleAnalytics />
+          {/* <CookieConsent
+            location="bottom"
+            buttonText="同意する"
+            declineButtonText="拒否"
+            cookieName="ga-consent"
+            style={{ background: "#2B373B" }}
+            buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+            expires={150}
+          >
+            このウェブサイトはCookieを使用してユーザー体験を向上させています。
+          </CookieConsent> */}
         </Providers>
       </body>
     </html>
