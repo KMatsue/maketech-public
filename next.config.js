d8199c187fb287527e2b32804051a66b046f440e
make <matsue4692@gmail.com>
2025-03-20 10:04:05 +0900

    update: next.config.js /tools/responsive-testerのパスのページをiFrameで表示できないように設定

--- next.config.js
+++ next.config.js
@@ -10,6 +10,23 @@ const nextConfig = {
       "prod-files-secure.s3.us-west-2.amazonaws.com",
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/tools/responsive-tester",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none'",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
